f4a5542c32d1fae4b167d06049d1c99761c01051
vkondratiukv <kondratyukvvv@gmail.com>
2023-11-05 13:00:43 +0200

    avarage

---
+++ main.js
@@ -0,0 +1,4 @@
+let firstNumber = Number(prompt("Введіть перше число: "));
+let secondNumber = Number(prompt("Введіть друге число: "));
+let thirdNumber = Number(prompt("Введіть третє число: "));
+alert(`Середнє арифметичне введених чисел: ${(firstNumber + secondNumber + thirdNumber) / 3}`);
